6867148ad8c86221e98608972db73aef0142117d
Wolf <183492338+AzureHound@users.noreply.github.com>
2025-06-29 14:08:33 +0530

    ♻️ refactor(bash): env vars reworked

--- .config/bash/env.sh
+++ .config/bash/env.sh
@@ -5,41 +5,23 @@ export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_BIN_HOME="$HOME/.local/bin"
 
-# XDG User Directories
-XDG_DESKTOP_DIR="$HOME/Desktop"
-XDG_DOCUMENTS_DIR="$HOME/Documents"
-XDG_DOWNLOAD_DIR="$HOME/Downloads"
-XDG_MUSIC_DIR="$HOME/Music"
-XDG_PICTURES_DIR="$HOME/Pictures"
-XDG_PUBLICSHARE_DIR="$HOME/Public"
-XDG_TEMPLATES_DIR="$HOME/Templates"
-XDG_VIDEOS_DIR="$HOME/Videos"
-
 # Respect XDG Specification
 export ATUIN_PATH="$HOME/.atuin/bin"
 export PATH="$ATUIN_PATH:$XDG_BIN_HOME:$PATH"
+export ANDROID_USER_HOME="$XDG_DATA_HOME/android"
+export CARGO_HOME="$XDG_DATA_HOME/cargo"
+export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
+export DOTNET_CLI_HOME="$XDG_DATA_HOME/dotnet"
+export FFMPEG_DATADIR="$XDG_CONFIG_HOME/ffmpeg"
 export GOPATH="$XDG_DATA_HOME/go"
 export GOMODCACHE="$XDG_CACHE_HOME/go/mod"
-export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
-export CARGO_HOME="$XDG_DATA_HOME/cargo"
-export ANDROID_USER_HOME="$XDG_DATA_HOME/android"
-export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
-export RIPGREP_CONFIG_PATH="$HOME/.config/rg/.ripgreprc"
-export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
-export FFMPEG_DATADIR="$XDG_CONFIG_HOME/ffmpeg"
-export PYENV_ROOT="$XDG_DATA_HOME/pyenv"
-export XINITRC="$XDG_CONFIG_HOME/X11/xinitrc"
-export XSERVERRC="$XDG_CONFIG_HOME/X11/xserverrc"
-export WINEPREFIX="$XDG_DATA_HOME/wine"
 export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
-export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc"
-export W3M_DIR="$XDG_DATA_HOME/w3m"
-export DOTNET_CLI_HOME="$XDG_DATA_HOME/dotnet"
 export PNPM_HOME="$XDG_DATA_HOME/pnpm"
-
-# Colors
-export LS_COLORS="$(vivid generate catppuccin-macchiato)"
-export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+export PYENV_ROOT="$XDG_DATA_HOME/pyenv"
+export RIPGREP_CONFIG_PATH="$HOME/.config/rg/.ripgreprc"
+export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
+export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
+export W3M_DIR="$XDG_DATA_HOME/w3m"
 
 # Editor
 export EDITOR=nvim
@@ -68,3 +50,7 @@ export MANPAGER="vim -M +MANPAGER -"
 NPM_PACKAGES="${HOME}/.npm-packages"
 export PATH="$PATH:$NPM_PACKAGES/bin"
 export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"
+
+# Vivid
+export LS_COLORS="$(vivid generate catppuccin-macchiato)"
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
